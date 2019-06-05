9b7e3d90a38788651c2f5b65b85dd5009045ef7f
Shunsei <khan.sufyan.cs@gmail.com>
2019-06-05 09:01:13 -0400

    Added custom stylesheet

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.set("useNewUrlParser", "true");
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 // ROUTES 
 
